40d814fda7918e5a2f8f86f8766d45e012cf1616
d.shiriaev <d.shiriaev@bars.group>
2023-01-16 06:48:02 +0400

    Убрал цвета

--- pages/project.vue
+++ pages/project.vue
@@ -1,9 +1,9 @@
 <template>
     <div class="bg-black h-screen flex-col">
-        <div class="bg-blue-400 w-full h-40">
+        <div class="w-full h-40">
             <div class="container mx-auto">Шапка проекта</div>
         </div>
-        <div class="bg-red-400 w-full h-16">
+        <div class="w-full h-16">
             <div class="container mx-auto flex flex-row justify-center gap-5 h-full">
                 <template v-for="index in tabs">
                     <div class="flex items-center hover:border-white hover:border-b-2 hover:cursor-pointer" @click="setCmp(index.cmp)">
@@ -12,7 +12,7 @@
                 </template>
             </div>
         </div>
-        <div class="bg-yellow-300 w-full h-[calc(100%-14rem)] overflow-y-auto">
+        <div class="w-full h-[calc(100%-14rem)] overflow-y-auto">
             <div class="container mx-auto mt-2">
                 <component :is="currentCmp" />
             </div>
